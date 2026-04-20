a57f68d325a41c51eefe5437f53ba17c0ac325fa
Rohit Prajapati <rohitprajapati232224@gmail.com>
2026-04-20 23:54:04 +0530

    Add task routes for CRUD operations

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -4,12 +4,20 @@ const router = express.Router();
 const controller = require("../controllers/taskController");
 const validateTask = require("../middleware/validation");
 
+
 router.post("/", validateTask, controller.createTask);
+
+
 router.get("/", controller.getTasks);
+
 router.get("/search", controller.searchTasks);
 
+
 router.put("/:id", validateTask, controller.updateTask);
+
+
 router.patch("/:id/status", controller.updateStatus);
+
 
 router.delete("/:id", controller.deleteTask);
 
